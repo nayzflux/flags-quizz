5b0382d1b6f6e99705bd85d50b6175f3f0bbc500
NayZ.Sodium <null>
2023-07-09 14:55:10 +0200

    Responsive mobile

--- src/App.jsx
+++ src/App.jsx
@@ -89,28 +89,32 @@ function App() {
     }
 
     return (
-        <main className="p-10 text-white flex flex-col justify-center items-center gap-8 w-full h-screen bg-gray-900">
+        <main className="p-5 md:p-10 text-white flex flex-col justify-center items-center gap-4 md:gap-8 w-full h-screen bg-gray-900">
             {
                 index === 216 ?
-                    <div className="font-bold text-4xl">
+                    <div className="font-bold text-2xl md:text-4xl">
                         Votre Score est de {score}/{tableau.length}
                     </div>
                 :
                     <>
-                        <h1 className="text-2xl font-semibold">Quizz Drapeaux du Monde</h1>
+                        <h1 className="text-lg md:text-2xl font-semibold">Quizz Drapeaux du Monde</h1>
 
-                        <img className="h-[400px]" alt="Drapeau du pays mystère" src={current?.fields?.flag}/>
+                        <img className="h-[200px] md:h-[400px]" alt="Drapeau du pays mystère" src={current?.fields?.flag}/>
                         <p className="font-semibold hidden">{current?.fields?.name_fr}</p>
 
-                        <div className="flex gap-4 w-full items-center">
+                        <div className="flex gap-2 md:gap-4 w-full">
                             <div className="p-3 bg-blue-600 rounded shadow hover:shadow-lg hover:shadow-blue-600 transition">
                                 <span>{score}/{index + 1}</span>
                             </div>
 
-                            <form onSubmit={handleSubmit} className="flex gap-4 w-full items-center">
+                            <form onSubmit={handleSubmit} className="flex flex-col md:flex-row gap-2 md:gap-4 w-full items-center">
                                 <input className="w-full p-3 rounded focus:outline-blue-600 shadow focus:shadow-lg transition focus:shadow-blue-600 outline outline-2 outline-neutral-300 bg-gray-700 text-white placeholder:text-white" value={name} placeholder={"Nom du pays (en FR)..."} onChange={(e) => setName(e.target.value)}/>
-                                <button className="ml-auto px-5 py-2.5 bg-blue-600 rounded shadow hover:shadow-lg hover:shadow-blue-600 transition">Valider</button>
-                                <button type="button" className="ml-auto px-5 py-2.5 bg-red-600 rounded shadow hover:shadow-lg hover:shadow-red-600 transition" onClick={skip}>Passer</button>
+
+                                <div className="flex gap-2 md:gap-4 ">
+                                    <button className="ml-auto px-5 py-2.5 bg-blue-600 rounded shadow hover:shadow-lg hover:shadow-blue-600 transition">Valider</button>
+                                    <button type="button" className="ml-auto px-5 py-2.5 bg-red-600 rounded shadow hover:shadow-lg hover:shadow-red-600 transition" onClick={skip}>Passer</button>
+                                </div>
+
                             </form>
                         </div>
 
